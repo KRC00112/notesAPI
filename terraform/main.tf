c099f1658e83bd9015454cb70309c31ae5548f05
unknown <nylonstripe5u@gmail.com>
2026-05-29 18:35:13 +0530

    eks cluster configured through terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,74 +1,251 @@
 provider "aws" {
-  region = "us-west-2"
+  region = var.region
 }
 
-data "aws_ami" "ubuntu" {
-  most_recent = true
+data "aws_availability_zones" "available" {
   filter {
-    name   = "name"
-    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
   }
-  owners = ["099720109477"]
 }
 
-
-resource "aws_security_group" "notesapi_sg" {
-  name        = "notesapi_sg"
-  description = "notesAPI security group"
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-  ingress {
-    from_port   = 6443
-    to_port     = 6443
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-
-    from_port   = 30000
-    to_port     = 32767
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    self      = true
-  }
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-
-  }
-
-
+locals {
+  cluster_name = "noteapi-eks-${random_string.suffix.result}"
 }
 
-resource "aws_key_pair" "notesapi_key" {
-  key_name   = "notesapi-key"
-  public_key = file("~/.ssh/notesapi-key.pub")
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
 }
 
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.8.1"
 
-resource "aws_instance" "app_server" {
-  count                  = 2
-  ami                    = data.aws_ami.ubuntu.id
-  instance_type          = "c7i-flex.large"
-  key_name               = aws_key_pair.notesapi_key.key_name
-  vpc_security_group_ids = [aws_security_group.notesapi_sg.id]
+  name = "notesapi-vpc"
 
-  tags = {
-    Name = "notes-api-instance-${count.index}"
+  cidr = "10.0.0.0/16"
+  azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+  enable_nat_gateway   = true
+  single_nat_gateway   = true
+  enable_dns_hostnames = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
+module "eks" {
+  source  = "terraform-aws-modules/eks/aws"
+  version = "20.8.5"
+
+  cluster_name    = local.cluster_name
+  cluster_version = "1.29"
+
+  cluster_endpoint_public_access           = true
+  enable_cluster_creator_admin_permissions = true
+
+  cluster_addons = {
+    aws-ebs-csi-driver = {
+      service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+    }
+  }
+
+  vpc_id     = module.vpc.vpc_id
+  subnet_ids = module.vpc.private_subnets
+
+  eks_managed_node_group_defaults = {
+    ami_type = "AL2_x86_64"
 
   }
 
+  eks_managed_node_groups = {
+    one = {
+      name = "node-group-1"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+    }
+
+    two = {
+      name = "node-group-2"
+
+      instance_types = ["t3.small"]
+
+      min_size     = 1
+      max_size     = 2
+      desired_size = 1
+    }
+  }
 }
+
+data "aws_iam_policy" "ebs_csi_policy" {
+  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+}
+
+module "irsa-ebs-csi" {
+  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
+  version = "5.39.0"
+
+  create_role                   = true
+  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
+  provider_url                  = module.eks.oidc_provider
+  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
+  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
+}
+
+data "aws_eks_cluster" "cluster" {
+  name = module.eks.cluster_name
+}
+
+provider "kubernetes" {
+  host                   = data.aws_eks_cluster.cluster.endpoint
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args = [
+      "eks",
+      "get-token",
+      "--cluster-name",
+      data.aws_eks_cluster.cluster.name
+    ]
+  }
+}
+
+resource "kubernetes_deployment_v1" "notesapi-deployment" {
+  metadata {
+    name = "notesapi-deployment"
+    labels = {
+      App = "notesapi"
+    }
+  }
+
+  spec {
+    replicas = 2
+    selector {
+      match_labels = {
+        App = "notesapi"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "notesapi"
+        }
+      }
+      spec {
+        container {
+          image = "krcdocker2138/notes-backend"
+          name  = "notesapi-container"
+
+          port {
+            container_port = 5000
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "notesapi-service" {
+  metadata {
+    name = "notesapi-service"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment_v1.notesapi-deployment.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 5000
+      target_port = 5000
+    }
+
+    type = "LoadBalancer"
+  }
+}
+
+resource "kubernetes_deployment_v1" "postgres-deployment" {
+  metadata {
+    name = "postgres-deployment"
+    labels = {
+      App = "postgres"
+    }
+  }
+
+  spec {
+    replicas = 1
+    selector {
+      match_labels = {
+        App = "postgres"
+      }
+    }
+    template {
+      metadata {
+        labels = {
+          App = "postgres"
+        }
+      }
+      spec {
+        container {
+          image = "postgres:15"
+          name  = "postgres-container"
+
+          port {
+            container_port = 5432
+          }
+          env {
+            name  = "POSTGRES_USER"
+            value = "postgres"
+          }
+          env {
+            name  = "POSTGRES_PASSWORD"
+            value = "1234"
+          }
+          env {
+            name  = "POSTGRES_DB"
+            value = "notesdb"
+          }
+          volume_mount {
+            name       = "pgdata"
+            mount_path = "/var/lib/postgresql/data"
+          }
+        }
+        volume {
+          name = "pgdata"
+          host_path {
+            path = "/mnt/pgdata"
+            type = "DirectoryOrCreate"
+          }
+        }
+      }
+    }
+  }
+}
+
+resource "kubernetes_service_v1" "postgres-service" {
+  metadata {
+    name = "postgres"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment_v1.postgres-deployment.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      port        = 5432
+      target_port = 5432
+    }
+  }
+}
+
--- terraform/terraform.tf
+++ terraform/terraform.tf
@@ -1,16 +1,30 @@
 terraform {
   required_providers {
-
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 5.92"
-
+      version = "~> 5.47.0"
     }
 
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.6.1"
+    }
+
+    tls = {
+      source  = "hashicorp/tls"
+      version = "~> 4.0.5"
+    }
+
+    cloudinit = {
+      source  = "hashicorp/cloudinit"
+      version = "~> 2.3.4"
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = "~> 3.0"
+    }
   }
 
-  required_version = ">=1.2"
-
+  required_version = "~> 1.3"
 }
-
 
